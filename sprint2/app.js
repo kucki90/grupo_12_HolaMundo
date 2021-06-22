46485932ab3e76eecf71201241b55527c2d32059
Davidruizf <dcruizf@gmail.com>
2021-06-22 13:30:29 -0300

    and structurc inicial

---
+++ sprint2/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const views = path.join(__dirname, 'views');
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(views, 'home.html')));
+
+app.listen(port, () => console.log('Server running in port ' + port))
