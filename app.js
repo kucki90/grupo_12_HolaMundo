e7635428085ae2caba50268d2301d4b1ef68a0e7
kucki90 <c.kuckiewicz@hotmail.com>
2021-06-29 20:18:48 -0300

    creación de detail

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(views, 'home.html')));
+app.get('/detail', (req,res) => res.sendFile(path.join(views, 'detail.html')));
 
 app.listen(port, () => console.log('Server running in port ' + port))
