61a70a30912be315f307e4c01dd207dc01a884f2
Davidruizf <dcruizf@gmail.com>
2021-08-10 11:56:13 -0300

    modificar producto mostrado por pantalla

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -1,14 +1,20 @@
+const fs = require('fs');
+const path = require('path')
 const productos = require('../data/products_db');
 const categorias = require('../data/categories_db');
 
 
 module.exports = {
+  
     add : (req,res) => {
         return res.render('productAdd',{
             categorias,
             productos
         })
     },
+    // save :(req, res) =>{
+    //   res.send(req.body)
+    // },
     
     detail : (req,res) => {
         let producto = productos.find(producto => producto.id === +req.params.id);
--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -4,18 +4,6 @@ const router = express.Router();
 const {add,detail,edit,update} = require('../controllers/productsController');
 const multer =require('multer');
 const path = require('path');
-const storage = multer.diskStorage({
-    destination : (req,file,callback) => {
-        callback(null,'public/images/productos')
-    },
-    filename : (req,fole,callback)=>{
-        callback(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
-
-const upload =multer ({
-    storage,
-})
 
 
 
