4941fc8a887abead5cd7f948edc96feb924b90d4
jenniferantunez4 <jenniferantunez4@gmail.com>
2021-08-11 14:59:18 -0300

    eliminacion terminado

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,8 +1,12 @@
 let productos = require('../data/products_db');
-
+const fs = require('fs');
+const path = require('path');
 module.exports = {
     home: (req, res) => {
+
+        productos = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', "products.json"), 'utf-8'));
         return res.render('home', {
+
             productos,
             oferta: productos.filter(producto => producto.category === "oferta"),
             destacado: productos.filter(producto => producto.category === "destacado"),
