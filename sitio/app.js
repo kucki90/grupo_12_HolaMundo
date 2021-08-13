8dfc671c75b4c5c9d29c16c9f392f9090628d596
kucki90 <c.kuckiewicz@hotmail.com>
2021-08-12 22:25:48 -0300

    devolucion de imagenes al banner, id en ofertas, muebles, deco

--- sitio/app.js
+++ sitio/app.js
@@ -23,8 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(methodOverride('_method'));
 
-app.use(methodOverride('_method'));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
