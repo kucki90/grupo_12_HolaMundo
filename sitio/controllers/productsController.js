80556817398924bae520fe8ceec4c37b9c87b3e9
jenniferantunez4 <jenniferantunez4@gmail.com>
2021-08-25 22:01:32 -0300

    validaciones en vista agregar/editar producto

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -1,112 +1,116 @@
 const fs = require('fs');
 const path = require('path')
-const {productos, guardar} = require('../data/products_db');
+const { productos, guardar } = require('../data/products_db');
 const categorias = require('../data/categories_db');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 
 
 module.exports = {
-    create : (req,res) => {
-       return res.render('productAdd',{
-            categorias, 
+    create: (req, res) => {
+        return res.render('productAdd', {
+            categorias,
             productos
         })
     },
-    
-    store : (req, res) => {
+
+    store: (req, res) => {
         const validacion = validationResult(req)
-        
-        if(validacion.errors.length>0){
+
+        if (validacion.errors.length > 0) {
             const locals = {
                 categorias,
                 productos,
-                old:req.body,
-                errors:validacion.mapped()
+                old: req.body,
+                errors: validacion.mapped()
             }
+
             return res.render('productAdd', locals)
-        } 
+        }
         let imagenes = [];
-        if (req.files){
+        if (req.files) {
             req.files.forEach(imagen => {
                 imagenes.push(imagen.filename)
             })
         }
-        
-		let producto = {
-			id:productos[productos.length-1].id+1,
-			title: req.body.title,
-			price: +req.body.price,
-			images: imagenes,
-			category: req.body.category,
-			description:req.body.description,
-		};
-		productos.push(producto)
-		fs.writeFileSync(path.join(__dirname,'../data/products.json'),JSON.stringify(productos))
-		res.redirect('/')
-	},
-    detail : (req,res) => {
+
+        let producto = {
+            id: productos[productos.length - 1].id + 1,
+            title: req.body.title,
+            price: +req.body.price,
+            images: imagenes,
+            category: req.body.category,
+            description: req.body.description,
+        };
+        productos.push(producto)
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos))
+        res.redirect('/')
+    },
+    detail: (req, res) => {
         let producto = productos.find(producto => producto.id === +req.params.id);
 
-        return res.render('productDetail',{
+        return res.render('productDetail', {
             producto,
             productos
         })
     },
-    edit : (req, res) => {
+    edit: (req, res) => {
         let producto = productos.find(producto => producto.id === +req.params.id);
 
-        return res.render('productEdit',{
+        return res.render('productEdit', {
             categorias,
             producto
         })
     },
-    update : (req, res) => {
+    update: (req, res) => {
         const validacion = validationResult(req)
         let producto = productos.find(producto => producto.id === +req.params.id)
-      
+
         // res.send(req.body)
-        const {title, description,price, category} = req.body;
-        
-        if(validacion.errors.length>0){
+        const { title, description, price, category } = req.body;
+
+        if (validacion.errors.length > 0) {
             const locals = {
                 categorias,
                 producto,
-                old:req.body,
-                errors:validacion.mapped()
+                old: req.body,
+                errors: validacion.mapped()
             }
-            return res.send(validacion.errors)
-        } 
-        if(req.files.length !=0){
+            console.log(validacion.mapped())
+
+            return res.render('productEdit', locals)
+
+        }
+        if (req.files.length != 0) {
             var imagenes = req.files.map(imagen => imagen.filename)
         }
-       
+
         let productoEditado = {
-            id : +req.params.id,
+            id: +req.params.id,
             title,
             description,
             category,
-            price : +price,
-            images : req. files.length !=0 ? imagenes : producto.images
-            
+            price: +price,
+            images: req.files.length != 0 ? imagenes : producto.images
+
         }
 
         let productosModificados = productos.map(producto => producto.id === +req.params.id ? productoEditado : producto)
-      
+
         guardar(productosModificados)
         res.redirect('/')
-        },
+    },
 
-    destroy :(req,res) => {
+    destroy: (req, res) => {
         let productoModificado = productos.filter(producto => producto.id != +req.params.id)
         guardar(productoModificado)
-     res.redirect('/')
+        res.redirect('/')
     },
-    search : (req, res) => {
-        let resultado = productos.filter(producto => producto.title.toLowerCase().includes(req.query.keywords.toLowerCase())||producto.description.toLowerCase().includes(req.query.keywords.toLowerCase()))
-        return res.render('resultado',{
-            productos : resultado,
-            keywords : req.query.keywords
+    search: (req, res) => {
+        let resultado = productos.filter(producto => producto.title.toLowerCase().includes(req.query.keywords.toLowerCase()) || producto.description.toLowerCase().includes(req.query.keywords.toLowerCase()))
+        return res.render('resultado', {
+            productos: resultado,
+            keywords: req.query.keywords
         })
     }
 }
