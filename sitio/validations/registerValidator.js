78262dec3a4296f89c84d7a4b09e98ad319262e0
DavidRuizf <dcruizf@gmail.com>
2021-08-27 11:15:45 -0300

    register

--- sitio/validations/registerValidator.js
+++ sitio/validations/registerValidator.js
@@ -9,6 +9,14 @@ module.exports = [
     }).withMessage('El nombre tiene que tener como mínimo 2 caracteres').bail()
     .isAlpha().withMessage('El nombre debe contener solo letras'),
 
+    check('apellido')
+    .notEmpty().withMessage('El apellido es obligatorio').bail()
+    .isLength({
+        min : 2,
+        max : 80
+    }).withMessage('El apellido tiene que tener como mínimo 2 caracteres').bail()
+    .isAlpha().withMessage('El el apellido debe contener solo letras'),
+
     check('email')
     .isEmail().withMessage('Debes ingresar un email válido'),
 
