f0e88e2741b8c0d48874fe455e4ff5212dcd7c72
kucki90 <c.kuckiewicz@hotmail.com>
2021-09-08 18:53:45 -0300

    agrega metodo a la vista

--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {register,login, processRegister, processLogin, logout} = require('../controllers/usersController');
+const {register,login, processRegister, processLogin, logout, profile} = require('../controllers/usersController');
 
 const registerValidator = require('../validations/registerValidator');
 const loginValidator =require('../validations/loginValidator');
