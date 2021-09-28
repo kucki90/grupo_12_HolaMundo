e1bd64d9032d855361f9e5523cfe1cd47c0dca01
jenniferantunez4 <jenniferantunez4@gmail.com>
2021-09-27 21:06:50 -0300

    profes me pueden aprobar??? jajajajajajajaja

--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {home,carrito} = require('../controllers/indexController')
+const {home,carrito,admin} = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', home);
 router.get('/carrito', carrito)
+router.get('/admin',admin)
 
 module.exports = router;
