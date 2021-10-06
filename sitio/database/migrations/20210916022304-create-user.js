f19ac99becf2204c61fa569ede87a436263b234f
Gonzalo Horacio Emanuel Gonzalez <gonzalo220201@gmail.com>
2021-10-05 22:22:45 -0300

    modifica sql y migraciones

--- sitio/database/migrations/20210916022304-create-user.js
+++ sitio/database/migrations/20210916022304-create-user.js
@@ -41,11 +41,11 @@ module.exports = {
       },
 
       createdAt: {
-        allowNull: false,
+      
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+      
         type: Sequelize.DATE
       }
     });
