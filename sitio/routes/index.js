1c7a7361a3cfc0e3ac1494fb311d1e2f9b6f6ca0
kucki90 <c.kuckiewicz@hotmail.com>
2021-11-03 15:27:11 -0300

    vista about funcionando y ajustes en la barra search

--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {home,carrito,admin,contact} = require('../controllers/indexController')
+const {home,carrito,admin,contact, about, history, work} = require('../controllers/indexController')
 const {verifyPassword} = require('../controllers/apiController')
 /* GET home page. */
 router.get('/', home);
@@ -9,5 +9,8 @@ router.get('/carrito', carrito)
 router.get('/admin',admin)
 router.post('/veryfy-password',verifyPassword)
 router.get('/contact', contact)
+router.get('/about', about)
+router.get('/history', history)
+router.get('/work', work)
 
 module.exports = router;
