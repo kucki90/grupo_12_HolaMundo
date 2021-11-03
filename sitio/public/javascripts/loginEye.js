3909c6047f5a5d242d025fc44ea226d098a1ec42
DavidRuizf <dcruizf@gmail.com>
2021-11-03 16:05:26 -0300

    eyes

---
+++ sitio/public/javascripts/loginEye.js
@@ -0,0 +1,27 @@
+function mostrarContrasena(){
+    var tipo = document.getElementById("password");
+    if(tipo.type == "password"){
+        tipo.type = "text";
+    }else{
+        tipo.type = "password";
+    }
+}
+
+
+// const iconEye =document.querySelector(".icon-eye");
+
+//  iconEye.addEventListener("click", function() {
+//      const icon = this.querySelector("i");
+  
+//      if(this.nextElementSibling.type === "password") {
+//          this.nextElementSibling.type = "text";
+//          icon.classList.remove("fa-eye-slash");
+//          icon.classList.add("fa-eye");
+//      } else {
+//          this.nextElementSibling.type = "password";
+//          icon.classList.remove("fa-eye");
+//          icon.classList.add("fa-eye-slash");
+//      }
+
+
+//  });
